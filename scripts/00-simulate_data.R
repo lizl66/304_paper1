a1489f0f0f540c5eff58a1173233209068016c9d
laijiaqi <163157996+lizl66@users.noreply.github.com>
2024-09-24 19:51:13 -0400

    00-simulate_data.R

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -18,10 +18,6 @@ set.seed(304)
 ### define the number of records to stimulate ###
 number_of_record <- 1000
 
-### define the date ###
-start_date <- as.Date("2018-01-01")
-end_date <- as.Date("2023-12-31")
-
 ###stimulate race bias###
 race_bias1 <- c("Black", "White", "Asian", "None")
 race_bias <- sample(race_bias1, number_of_record, replace = TRUE)
@@ -34,12 +30,18 @@ sexual_orientation <- sample(sexual_orientation1, number_of_record, replace = TR
 primary_offense1 <- c("Wilful Promotion of Hatred", "Assault", "Mischief Under $5000", "Uttering Threats", "Criminal Harassment", "Theft", "Indecent Communications") 
 primary_offense <- sample(primary_offense, number_of_record, replace = TRUE)
 
-###stimulate whether the assult was made###
-assault_made <- sample(c("YES", "NO"), number_of_record, replace = TRUE, prob = c(0.3, 0.7))
+###stimulate whether arrest was made###
+arrest_made <- sample(c("YES", "NO"), number_of_record, replace = TRUE, prob = c(0.3, 0.7))
 
 ###stimulated data frame###
 stimulated_data <- tibble(
   id = 1:number_of_record,
-  
+  RACE_BIAS = race_bias,
+  SEXUAL_ORIENTATION = sexual_orientation,
+  PRIMARY_OFFENSE = primary_offense,
+  ARREST_MADE = arrest_made
+)
+
+write_csv(simulated_data, file = "data/raw_data/simulated.csv")
 
 
