764fb556d8363062f9e7ccb4d6be30e226cb8ea3
laijiaqi <163157996+lizl66@users.noreply.github.com>
2024-09-24 19:27:39 -0400

    00-simulate_data.R

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -16,11 +16,30 @@ library(tidyverse)
 set.seed(304)
 
 ### define the number of records to stimulate ###
-number_of_record <- 300
+number_of_record <- 1000
 
 ### define the date ###
 start_date <- as.Date("2018-01-01")
 end_date <- as.Date("2023-12-31")
 
+###stimulate race bias###
+race_bias1 <- c("Black", "White", "Asian", "None")
+race_bias <- sample(race_bias1, number_of_record, replace = TRUE)
+
+###stimulate sexual orientation bias###
+sexual_orientation1 <- c("Gay", "Lesbian", "2SLGBTQ+")
+sexual_orientation <- sample(sexual_orientation1, number_of_record, replace = TRUE)
+
+###stimulate primary offense###
+primary_offense1 <- c("Wilful Promotion of Hatred", "Assault", "Mischief Under $5000", "Uttering Threats", "Criminal Harassment", "Theft", "Indecent Communications") 
+primary_offense <- sample(primary_offense, number_of_record, replace = TRUE)
+
+###stimulate whether the assult was made###
+assault_made <- sample(c("YES", "NO"), number_of_record, replace = TRUE, prob = c(0.3, 0.7))
+
+###stimulated data frame###
+stimulated_data <- tibble(
+  id = 1:number_of_record,
+  
 
 
